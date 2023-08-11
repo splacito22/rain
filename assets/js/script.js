c5f62c03f7a20ac07da6a50f259f6733ac31f7d2
Stephany <136421961+splacito22@users.noreply.github.com>
2023-08-10 19:18:49 -0700

    init commit. added files

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function getCoordinates(cityName) {
+  var apiKey = "d3eb013621b045498080b3dca1ba23a8";
+  var geocodingUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}";
+}
